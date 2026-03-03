de594a572664e3ce68cf02a1e67a8d3f30df1cb0
Deepanshu Yadav <65827314+deeps47@users.noreply.github.com>
2026-03-03 12:03:49 +0530

    added bibata cursor

--- modules/theme.nix
+++ modules/theme.nix
@@ -4,52 +4,60 @@
   home.packages = with pkgs; [
     adwaita-icon-theme
     gnome-themes-extra
+    bibata-cursors
   ];
 
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
+  # 1. The "Master" Cursor Configuration
+  # This handles X11, Wayland, and creates the necessary ~/.icons/default/index.theme
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic"; # Change to "Bibata-Modern-Ice" for white
+    size = 24;
   };
-  
+
+  # 2. GTK Settings
   gtk = {
     enable = true;
     theme.name = "Adwaita-dark";
-    iconTheme.name = "Adwaita-dark";
-    cursorTheme = {
-      name = "Adwaita-dark";
-      package = pkgs.adwaita-icon-theme;
-      size = 24;
+    iconTheme.name = "Adwaita";
+    # cursorTheme is automatically handled by home.pointerCursor above
+  };
+
+  # 3. GNOME/Desktop Interface Settings
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      cursor-theme = "Bibata-Modern-Classic";
+      cursor-size = 24;
     };
   };
 
+  # 4. Qt Settings
   qt = {
     enable = true;
     platformTheme.name = "adwaita";
     style.name = "adwaita-dark";
   };
-  
+
+  # 5. Manual GTK overrides (Simplified)
+  # home-manager handles the cursor lines now, so we only need the theme preference
   xdg.configFile."gtk-3.0/settings.ini".text = ''
     [Settings]
     gtk-theme-name=Adwaita-dark
-    gtk-icon-theme-name=Adwaita-dark
-    gtk-cursor-theme-name=Adwaita-dark
-    gtk-cursor-theme-size=24
     gtk-application-prefer-dark-theme=1
   '';
 
   xdg.configFile."gtk-4.0/settings.ini".text = ''
     [Settings]
     gtk-theme-name=Adwaita-dark
-    gtk-icon-theme-name=Adwaita-dark
-    gtk-cursor-theme-name=Adwaita-dark
-    gtk-cursor-theme-size=24
     gtk-application-prefer-dark-theme=1
   '';
 
+  # 6. Session Variables
   home.sessionVariables = {
-    XCURSOR_THEME = "Adwaita-dark";
+    XCURSOR_THEME = "Bibata-Modern-Classic";
     XCURSOR_SIZE = "24";
   };
 }
-
