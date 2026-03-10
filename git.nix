9bb6859bf62718686d03ded5cc6b3c938ae156ee
Deepanshu Yadav <65827314+deeps47@users.noreply.github.com>
2026-03-10 10:59:33 +0530

    updated git config

--- git.nix
+++ git.nix
@@ -2,6 +2,8 @@
 {
   programs.git = {
     enable = true;
+    name = "deeps47";
+    email = "65827314+deeps47@users.noreply.github.com";
     # Use the git package that has libsecret support
     package = pkgs.git.override { withLibsecret = true; };
     settings = {
