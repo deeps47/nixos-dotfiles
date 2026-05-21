38c7db2362815e64d62316922dbb61fb3c4883ad
Deepanshu Yadav <65827314+deeps47@users.noreply.github.com>
2026-05-22 01:49:43 +0530

    updated theme

--- modules/theme.nix
+++ modules/theme.nix
@@ -1,63 +1,69 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
-    adwaita-icon-theme
-    gnome-themes-extra
+    tokyonight-gtk-theme
+    papirus-icon-theme
     bibata-cursors
+    adwaita-qt
   ];
 
-  # 1. The "Master" Cursor Configuration
-  # This handles X11, Wayland, and creates the necessary ~/.icons/default/index.theme
-  home.pointerCursor = {
-    gtk.enable = true;
-    x11.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic"; # Change to "Bibata-Modern-Ice" for white
-    size = 24;
-  };
-
-  # 2. GTK Settings
   gtk = {
     enable = true;
-    theme.name = "Adwaita-dark";
-    iconTheme.name = "Adwaita";
-    # cursorTheme is automatically handled by home.pointerCursor above
+
+    theme = {
+      name = "Tokyonight-Dark";
+      package = pkgs.tokyonight-gtk-theme;
+    };
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    cursorTheme = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
+
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
 
-  # 3. GNOME/Desktop Interface Settings
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      cursor-theme = "Bibata-Modern-Classic";
-      cursor-size = 24;
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+    style = {
+      name = "adwaita-dark";
+      package = pkgs.adwaita-qt;
     };
   };
 
-  # 4. Qt Settings
-  qt = {
-    enable = true;
-    platformTheme.name = "adwaita";
-    style.name = "adwaita-dark";
+  dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+    gtk-theme = "Tokyonight-Dark";
+    icon-theme = "Papirus-Dark";
+    cursor-theme = "Bibata-Modern-Classic";
+    cursor-size = 24;
   };
 
-  # 5. Manual GTK overrides (Simplified)
-  # home-manager handles the cursor lines now, so we only need the theme preference
-  xdg.configFile."gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name=Adwaita-dark
-    gtk-application-prefer-dark-theme=1
-  '';
-
-  xdg.configFile."gtk-4.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name=Adwaita-dark
-    gtk-application-prefer-dark-theme=1
-  '';
-
-  # 6. Session Variables
   home.sessionVariables = {
-    XCURSOR_THEME = "Bibata-Modern-Classic";
-    XCURSOR_SIZE = "24";
+  # Session
+  #XDG_CURRENT_DESKTOP = "Hyprland"; # only downside — need to change this when switching to niri
+  XDG_SESSION_TYPE    = "wayland";
+  GDK_BACKEND         = "wayland,x11";
+
+  # Theme
+  GTK_THEME           = "Tokyonight-Dark";
+  GTK_ICON_THEME      = "Papirus-Dark";
+  XCURSOR_THEME       = "Bibata-Modern-Classic";
+  XCURSOR_SIZE        = "24";
   };
+
+  home.file.".icons/default/index.theme".text = ''
+    [Icon Theme]
+    Name=Default
+    Inherits=Bibata-Modern-Classic
+  '';
 }
