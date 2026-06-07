3f5c5a18d609742f10565c75812ba60e9295b3a5
Deepanshu Yadav <65827314+deeps47@users.noreply.github.com>
2026-06-07 06:00:52 +0530

    updated treesitter for neovim 0.12 compatibility

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,58 +1,52 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        main = "nvim-treesitter",
         build = ":TSUpdate",
         dependencies = {
-            "nvim-treesitter/nvim-treesitter-textobjects",
+            {
+                "nvim-treesitter/nvim-treesitter-textobjects",
+                branch = "main",
+                config = function()
+                    require("nvim-treesitter-textobjects").setup({
+                        select = { lookahead = true },
+                    })
+
+                    vim.keymap.set({ "x", "o" }, "af", function()
+                        require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+                    end, { desc = "Select outer function" })
+
+                    vim.keymap.set({ "x", "o" }, "if", function()
+                        require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+                    end, { desc = "Select inner function" })
+                end,
+            },
         },
-        config = function()
-            local configs = require("nvim-treesitter.configs")
-            ---@diagnostic disable-next-line: missing-fields
-            configs.setup({
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                        },
-                    },
-                },
-                -- enable syntax highlighting
-                highlight = {
-                    enable = true,
-                },
-                -- enable indentation
-                indent = { enable = true },
-                -- enable autotagging (w/ nvim-ts-autotag plugin)
-                autotag = { enable = true },
-                -- ensure these language parsers are installed
-                ensure_installed = {
-                    "json",
-                    "python",
-                    "javascript",
-                    "query",
-                    "typescript",
-                    "tsx",
-                    "php",
-                    "yaml",
-                    "html",
-                    "css",
-                    "markdown",
-                    "markdown_inline",
-                    "bash",
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "c",
-                    "dockerfile",
-                    "gitignore",
-                    "astro",
-                },
-                -- auto install above language parsers
-                auto_install = false,
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    pcall(vim.treesitter.start)
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
             })
-        end
-    }
+
+            local ensureInstalled = {
+                "json", "python", "javascript", "query",
+                "typescript", "tsx", "php", "yaml", "html",
+                "css", "markdown", "markdown_inline", "bash",
+                "lua", "vim", "vimdoc", "c", "dockerfile",
+                "gitignore", "astro",
+            }
+            local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+            local toInstall = vim.iter(ensureInstalled)
+                :filter(function(p)
+                    return not vim.tbl_contains(alreadyInstalled, p)
+                end)
+                :totable()
+            if #toInstall > 0 then
+                require("nvim-treesitter").install(toInstall)
+            end
+        end,
+    },
 }
--- neovim.nix
+++ neovim.nix
@@ -1,22 +1,23 @@
 { pkgs, ... }:
 {
-    home.packages = with pkgs; [
-      neovim
-      # LSP servers
-      lua-language-server
-      typescript-language-server
-      vscode-langservers-extracted
-      intelephense
-      nil
-      nixpkgs-fmt
+  home.packages = with pkgs; [
+    neovim
+    # LSP servers
+    lua-language-server
+    typescript-language-server
+    vscode-langservers-extracted
+    intelephense
+    nil
+    nixpkgs-fmt
+    tree-sitter
 
-      # Tools
-      lazygit
-      ripgrep # telescope grep
-      fd # telescope find
-      jq
-      fd
-      fzf
-      gcc
-    ];
+    # Tools
+    lazygit
+    ripgrep # telescope grep
+    fd # telescope find
+    jq
+    fd
+    fzf
+    gcc
+  ];
 }
